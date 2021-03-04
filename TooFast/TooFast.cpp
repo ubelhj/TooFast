958af12e57fdfcc11c0f395df32c4d143eeeed5f
ubelhj <ubelhj@uw.edu>
2021-03-03 17:02:44 -0800

    adds settings file and enable/disable features

--- TooFast/TooFast.cpp
+++ TooFast/TooFast.cpp
@@ -5,8 +5,10 @@
 BAKKESMOD_PLUGIN(TooFast, "Too Fast", plugin_version, PLUGINTYPE_THREADED)
 
 std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
-float speedMultiplier;
-int minSpeedThreshold;
+bool enableSpeedMultiplier = false;
+float speedMultiplier = 1.01;
+float speedThreshold = 2300.0;
+float maxSpeed = 2300.0;
 
 void TooFast::onLoad()
 {
@@ -38,14 +40,52 @@ void TooFast::onLoad()
 		}
 		});
 
-	cvarManager->registerCvar("too_fast_speed_multiplier", "1.0001", 
+	cvarManager->registerCvar("too_fast_speed_multiplier_enabled", "0", "Enable the speed multiplier", true, true, 0, true, 1)
+		.addOnValueChanged([this](std::string, CVarWrapper cvar) {
+			enableSpeedMultiplier = cvar.getBoolValue();
+			});
+
+	cvarManager->registerCvar("too_fast_speed_multiplier", "1.01", 
 			"Multiplies speed by this amount each tick", true, true, 1, true, 2)
 		.addOnValueChanged([this](std::string, CVarWrapper cvar) { speedMultiplier = cvar.getFloatValue(); });
 
-	cvarManager->registerCvar("too_fast_min_speed", "0", "Speed where the plugin begins to act", true, true, 0, false, 2300)
-		.addOnValueChanged([this](std::string, CVarWrapper cvar) { minSpeedThreshold = cvar.getFloatValue(); });
+	cvarManager->registerCvar("too_fast_speed_threshold", "2300", "Speed where the plugin stops increasing acceleration (negative to disable)", true, true, -1, false, 2300)
+		.addOnValueChanged([this](std::string, CVarWrapper cvar) { speedThreshold = cvar.getFloatValue(); });
+	
+	cvarManager->registerCvar("too_fast_max_speed", "2300", "Sets new max speed for the main player", true, true, 0, false, 2300)
+		.addOnValueChanged([this](std::string, CVarWrapper cvar) { maxSpeed = cvar.getFloatValue(); });
 
-	//cvarManager->registerNotifier("NOTIFIER", [this](std::vector<std::string> params){FUNCTION();}, "DESCRIPTION", PERMISSION_ALL);
+	//cvarManager->registerNotifier("too_fast_double_max_speed", [this](std::vector<std::string> params){ 
+	//	CarWrapper myCar = gameWrapper->GetLocalCar();
+
+	//	if (myCar.IsNull()) {
+	//		return;
+	//	}
+	//	//float maxAngularSpeed = myCar.GetMaxAngularSpeed();
+	//	//myCar.SetMaxAngularSpeed2(maxAngularSpeed * 2);
+	//	
+	//	cvarManager->log("new max speed " + std::to_string(myCar.GetMaxLinearSpeed()));
+	//	}, "Increases car's max speed", PERMISSION_ALL);
+	
+	cvarManager->registerNotifier("too_fast_get_max_speed", [this](std::vector<std::string> params) {
+		CarWrapper myCar = gameWrapper->GetLocalCar();
+
+		if (myCar.IsNull()) {
+			return;
+		}
+		cvarManager->log("max speed " + std::to_string(myCar.GetMaxLinearSpeed()));
+		}, "Increases car's max speed", PERMISSION_ALL);
+
+	/*cvarManager->registerNotifier("too_fast_explode", [this](std::vector<std::string> params) {
+		CarWrapper myCar = gameWrapper->GetLocalCar();
+
+		if (myCar.IsNull()) {
+			return;
+		}
+
+		myCar.Demolish();
+		
+		}, "Increases car's max speed", PERMISSION_ALL);*/
 	//cvarManager->registerCvar("CVAR", "DEFAULTVALUE", "DESCRIPTION", true, true, MINVAL, true, MAXVAL);//.bindTo(CVARVARIABLE);
 	//gameWrapper->HookEvent("FUNCTIONNAME", std::bind(&TEMPLATE::FUNCTION, this));
 	//gameWrapper->HookEventWithCallerPost<ActorWrapper>("FUNCTIONNAME", std::bind(&TooFast::FUNCTION, this, _1, _2, _3));
@@ -108,20 +148,36 @@ void TooFast::onTick(CarWrapper caller) {
 		return;
 	}
 
+	// mex speed is reset when demoed, so ensures it's always valid
+	myCar.SetMaxLinearSpeed2(maxSpeed);
+
 	auto controller = myCar.GetPlayerController();
 
 	auto input = controller.GetVehicleInput();
 	//cvarManager->log("Throttle : " + std::to_string(input.Throttle));
 
 	if (input.Throttle == 0) {
-		cvarManager->log("player is not throttling");
+		//cvarManager->log("player is not throttling");
 		return;
 	}
 
 	auto currentVelocity = myCar.GetVelocity();
 	float currSpeed = myCar.GetForwardSpeed();
+
+	if (!myCar.IsOnGround()) {
+		return;
+	}
+
+	if (!enableSpeedMultiplier) {
+		return;
+	}
+
+	if (std::abs(currSpeed) >= speedThreshold) {
+		return;
+	}
+
 	if ((currSpeed <= 0 && input.Throttle < 0) || (currSpeed >= 0 && input.Throttle > 0)) {
-		cvarManager->log("Curr speed :" + std::to_string(currSpeed));
+		//cvarManager->log("Curr speed :" + std::to_string(currSpeed));
 		currentVelocity *= speedMultiplier;
 		myCar.SetVelocity(currentVelocity);
 	}
